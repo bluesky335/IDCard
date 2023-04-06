d7689de9d382e6c48fde7f96e3af4bb8cc85e9b8
BlueSky335 <chinabluesky335@gmail.com>
2023-04-07 01:32:07 +0800

    添加创建身份证号码的功能

--- Sources/IDCard/IDCard.swift
+++ Sources/IDCard/IDCard.swift
@@ -1,27 +1,27 @@
 /*
-MIT License
+ MIT License
 
-Copyright (c) 2021 CoderX
+ Copyright (c) 2021 CoderX
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
 
-*/
+ */
 // Author: @BlueSky335 github home page : https://github.com/bluesky335/IDCard
 //
 //  IDCard.swift
@@ -39,56 +39,74 @@ public class IDCard {
         case female
     }
 
-    public struct Birthday {
+    public struct Birthday: Equatable {
         public let year: String
         public let month: String
         public let day: String
-        public let date: Date
+
+        public var date: Date {
+            let formater = DateFormatter()
+            formater.dateFormat = "yyyy-MM-dd"
+            return formater.date(from: "\(year)-\(month)-\(day)") ?? Date(timeIntervalSince1970: 0)
+        }
 
         public init(year: String, month: String, day: String) {
             self.year = year
             self.month = month
             self.day = day
-            let formater = DateFormatter()
-            formater.dateFormat = "yyyy-MM-dd"
-            date = formater.date(from: "\(year)-\(month)-\(day)") ?? Date(timeIntervalSince1970: 0)
         }
     }
-    
+
     /// 身份证号码
     public let numbers: String
-    
+
     /// 生日
     public private(set) lazy var birthday: Birthday = getBirthday()
-    
+
     /// 性别
     public private(set) lazy var gender: Gender = getGender()
-    
+
     /// 是否符合国标
     public private(set) lazy var isValid: Bool = checkIDCard()
-    
+
     /// 初始化
     /// - Parameter number: 身份证号码
     public init(number: String) {
-        self.numbers = number.uppercased()
+        numbers = number.uppercased()
     }
-    
-    /// 获取身份证号码
-    /// - Returns: 生日，如果是不符合规范的身份证，则返回时间戳为0的时间
-    private func getBirthday() -> Birthday {
-        guard self.isValid else {
-            return Birthday(year: "", month: "", day: "")
+
+    public class func createIDCardNumber(cityCode: String? = nil, gender: Gender? = nil, birthday: Date? = nil, dateFormater: DateFormatter? = nil) -> String {
+        var cityStr = cityCode ?? cityCodeList.randomElement() ?? "110105"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", "^[0-9]{6}$")
+        if !predicate.evaluate(with: cityStr) {
+            cityStr = "110105"
         }
-        let year = numbers[6 ..< 10]
-        let month = numbers[10 ..< 12]
-        let day = numbers[12 ..< 14]
-        return Birthday(year: String(year), month: String(month), day: String(day))
+        var randomNumber = Int.random(in: 100 ... 998)
+        if let g = gender {
+            switch g {
+            case .female:
+                if randomNumber % 2 != 0 {
+                    randomNumber += 1
+                }
+            case .male:
+                if randomNumber % 2 == 0 {
+                    randomNumber += 1
+                }
+            }
+        }
+        let randomStr = "\(randomNumber)"
+        let formater = dateFormater ?? DateFormatter()
+        formater.dateFormat = "yyyyMMdd"
+        let dateIntervel = Date().timeIntervalSince1970 - TimeInterval.random(in: 0 ... 1) * (70.0 * 365.0 * 24.0 * 60.0 * 60.0)
+        let birthdayDate = birthday ?? Date(timeIntervalSince1970: dateIntervel)
+        let birthDayStr = formater.string(from: birthdayDate)
+        var cardStr = cityStr + birthDayStr + randomStr
+        let signChar = getSignChar(for: cardStr + "0")
+        cardStr += signChar ?? "0"
+        return cardStr
     }
-    
-    // 检查是否符合身份证国标
-    // 计算规则参考“中国国家标准化管理委员会”官方文档：http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=080D6FBF2BB468F9007657F26D60013E
-    /// - Returns: 是否符合国标要求
-    private func checkIDCard() -> Bool {
+
+    private static func getSignChar(for numbers: String) -> String? {
         let a1Map: [Int: Int] = [
             0: 1,
             1: 0,
@@ -106,42 +124,60 @@ public class IDCard {
         let idStr = numbers.uppercased()
         let predicate = NSPredicate(format: "SELF MATCHES %@", "^[0-9]{17}[0-9X]$")
         guard predicate.evaluate(with: idStr) else {
-            return false
+            return nil
         }
 
         var sum: Int = 0
-        var signChar = ""
         for (index, c) in idStr.enumerated() {
             let i = 18 - index
-            if i != 1 {
+            if index != 17 {
                 guard let v = Int(String(c)) else {
-                    return false
+                    return nil
                 }
                 let weight = Int(pow(2, Float(i - 1))) % 11
 
                 sum += v * weight
-            } else {
-                signChar = String(c)
             }
         }
         guard let a1 = a1Map[sum % 11] else {
-            return false
+            return nil
         }
 
         var a1Str = "\(a1)"
         if a1 == 10 {
             a1Str = "X"
         }
+        return a1Str
+    }
+
+    /// 获取身份证号码
+    /// - Returns: 生日，如果是不符合规范的身份证，则返回时间戳为0的时间
+    private func getBirthday() -> Birthday {
+        guard isValid else {
+            return Birthday(year: "", month: "", day: "")
+        }
+        let year = numbers[6 ..< 10]
+        let month = numbers[10 ..< 12]
+        let day = numbers[12 ..< 14]
+        return Birthday(year: String(year), month: String(month), day: String(day))
+    }
+
+    // 检查是否符合身份证国标
+    // 计算规则参考“中国国家标准化管理委员会”官方文档：http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=080D6FBF2BB468F9007657F26D60013E
+    /// - Returns: 是否符合国标要求
+    private func checkIDCard() -> Bool {
+        let signChar = String(numbers[numbers.index(numbers.startIndex, offsetBy: 17)])
+        let a1Str = Self.getSignChar(for: numbers)
         guard a1Str == signChar else {
             return false
         }
         return true
     }
-    
+
     /// 获取性别
     /// - Returns: 性别
     private func getGender() -> Gender {
-        guard self.isValid else {
+        guard isValid else {
             return .male
         }
         let numStr = numbers[14 ..< 17]
--- Tests/IDCardTests/IDCardTests.swift
+++ Tests/IDCardTests/IDCardTests.swift
@@ -1,15 +1,23 @@
-import XCTest
 @testable import IDCard
+import XCTest
 
 final class IDCardTests: XCTestCase {
-    func testExample() {
+    func testIDCards() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(IDCard().text, "Hello, World!")
+        // 官方示例-女
+        testCard(cardnumber: "11010519491231002X", birthDay: .init(year: "1949", month: "12", day: "31"), gender: .female)
+        // 官方示例-男
+        testCard(cardnumber: "440524188001010014", birthDay: .init(year: "1880", month: "01", day: "01"), gender: .male)
+        let number = IDCard.createIDCardNumber()
+        XCTAssertTrue(IDCard(number: number).isValid)
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testCard(cardnumber: String, birthDay: IDCard.Birthday, gender: IDCard.Gender) {
+        let card = IDCard(number: cardnumber)
+        XCTAssertTrue(card.isValid)
+        XCTAssertEqual(card.gender, gender)
+        XCTAssertEqual(card.birthday, birthDay)
+    }
 }
